36bd3ede7b2f2225e6d1323b89b75a2c65a718e6
Bassma <bellaarbiahajar@gmail.com>
2025-08-03 14:40:29 +0200

    fix: small typo in class - now can properly select via app.js

--- src/app.js
+++ src/app.js
@@ -1,18 +1,42 @@
 const todayBtn = document.getElementById("today-btn");
-const weekBtn = document.querySelector("#week-btn");
 todayBtn.addEventListener("click", (event) => {
-  let content = document.querySelector("#content-dsiplayed");
-  content.style.display = "flex";
-});
-console.log(weekBtn);
-weekBtn.addEventListener("click", (event) => {
   event.preventDefault();
-  let content = document.querySelector("#content-dsiplayed");
-  content.style.display = "none";
+  let content = document.querySelector("#content-displayed");
+  let forecastSection = document.getElementById("forecast-section");
+  content.style.display = "flex";
+  forecastSection.style.display = "none";
 });
 
+function updateForecast(data) {
+  let forecastSection = document.getElementById("forecast-section");
+  let content = document.querySelector("#content-displayed");
+  content.style.display = "none";
+  forecastSection.style.display = "flex";
+}
+
+const weekBtn = document.querySelector("#week-btn");
+console.log(weekBtn);
+
+function forecastApi(city) {
+  let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  console.log(forecastApiUrl);
+  try {
+    fetch(forecastApiUrl).then((response) => {
+      let forecastResponse = response.json().then((data) => {
+        weekBtn.addEventListener("click", (e, data) => {
+          e.preventDefault();
+          updateForecast(data);
+        });
+      });
+    });
+  } catch (error) {
+    console.log("There's something wrong with the forecast API");
+    console.log(error.message);
+  }
+}
+
 function updateWeather(response) {
-  let city = document.getElementById("user-city-input");
+  let city = response.city;
   let time = response.time;
   let date = new Date(time * 1000);
   let formattedDate = moment(date).format("dddd LT");
@@ -34,15 +58,16 @@ function updateWeather(response) {
   let icon = document.querySelector("#current-temp-icon");
 
   console.log(weatherDetails);
-  firstHeading.innerHTML = response.city;
+  firstHeading.innerHTML = city;
   dateUpdated.innerHTML = `${formattedDate} – `;
   weatherDetails.innerHTML = weatherCondition;
   humidity.innerHTML = `Humidity: <strong>${humidityApi}%</strong> |`;
   wind.innerHTML = ` ${windApi}km/h `;
   tempNow.innerHTML = `${currentTemp} <div class="current-temp-metric"></div>`;
   icon.innerHTML = `  <img class="current-temp-icon" src="${iconApi}">`;
+  forecastApi(response.city);
 }
-let devMoode = true;
+let devMoode = false;
 async function apiHandler(city) {
   if (devMoode) {
     console.log(`I need to work on the CSS, stop calls for ${city}`);
@@ -50,8 +75,8 @@ async function apiHandler(city) {
   }
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
+    let response = await fetch(apiUrl);
+    let data = await response.json();
     updateWeather(data);
   } catch (error) {
     console.log("An Error in the API has been caught");
